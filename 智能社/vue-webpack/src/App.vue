608e7835e0a6126a47d70b7fc540750b51bec0d0
ZhangDasha <zhangdada315@gmail.com>
2017-12-08 19:17:21 +0800

    Add New Files

---
+++ 智能社/vue-webpack/src/App.vue
@@ -0,0 +1,40 @@
+<template id="home"> 
+  <!-- 这里放置HTML代码 -->
+  <div>
+    <ul @click="change()">
+      <li>111</li>
+      <li>222</li>
+      <li>{{msg}}</li>
+    </ul>
+    <hr>
+    <menu></menu>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+// 这里放置js代码
+export default {
+
+  id:'#home',
+  data() { 
+    return{
+      msg:"这是组件数据"
+    }
+  },
+  methods:{
+    change(){
+      console.log('This is vue use webpack');
+    }
+  }
+
+}
+</script>
+
+<style>
+/*这里放置css代码*/
+  body{
+    font-size: 20px;
+    color: red;
+  }
+</style>
---
+++ 智能社/vue-webpack/src/components/Menu.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+  	<p>这是组件Menu</p>
+  	<ol>
+  		<li>aaa</li>
+  		<li>bbb</li>
+  		<li>ccc</li>
+  	</ol>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
