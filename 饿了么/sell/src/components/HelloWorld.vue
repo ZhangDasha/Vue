5ae34a0d1b414259334927271fee57d0a3db6a5d
ZhangDasha <zhangdada315@gmail.com>
2017-12-14 19:21:49 +0800

    Add New Files

---
+++ 饿了么/sell/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <h3>{{msg}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: '仿饿了么'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
