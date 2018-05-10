9234b81322598c3f231adc88a3d1df2db358d7a0
ZhangDasha <zhangdada315@gmail.com>
2018-05-10 16:04:28 +0800

    排行主页面的实现

---
+++ Vue.js音乐app/vue-music/src/api/rank.js
@@ -0,0 +1,13 @@
+// 抓取排行榜
+import jsonp from 'common/js/jsonp'
+import {commonParams, options} from './config'
+
+export function getTopList() {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_myqq_toplist.fcg'
+
+  const data = Object.assign({}, commonParams, {
+    playform: 'h5',
+    needNewCode: 1
+  })
+  return jsonp(url, data, options)
+}
