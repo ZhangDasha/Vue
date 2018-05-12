6f1dcf162aac2a3ccc5a58f3ff75a9862e42630b
ZhangDasha <zhangdada315@gmail.com>
2018-05-12 21:13:44 +0800

    搜索页面
    1. 搜索框的实现
    2. 热门搜索
    3. 搜索项目的下拉刷新

--- Vue.js音乐app/vue-music/src/base/scroll/scroll.vue
+++ Vue.js音乐app/vue-music/src/base/scroll/scroll.vue
@@ -27,6 +27,10 @@
       listenScroll: {
         type: Boolean,
         default: false
+      },
+      pullup: {
+        type: Boolean,
+        default: false
       }
     },
     mounted() {
@@ -47,6 +51,14 @@
           let me = this
           this.scroll.on('scroll', (pos) => {
             me.$emit('scroll', pos)
+          })
+        }
+        // 下拉刷新
+        if (this.pullup) {
+          this.scroll.on('scrollEnd', () => {
+            if (this.scroll.y <= (this.scroll.maxScrollY + 50)) {
+              this.$emit('scrollToEnd')
+            }
           })
         }
       },
--- Vue.js音乐app/vue-music/src/common/js/song.js
+++ Vue.js音乐app/vue-music/src/common/js/song.js
@@ -30,7 +30,7 @@ export function createSong(musicData) {
   })
 }
 
-function filterSinger(singer) {
+export function filterSinger(singer) {
   let ret = []
   if (!singer) {
     return ''
